5781a74659176b847ce9e237a8facaba77b49d28
durimgashi <durimgashi97@gmail.com>
2020-06-03 15:44:52 +0200

    Small design changes

--- app/src/main/java/com/fiek/hapirri/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/fiek/hapirri/ui/slideshow/SlideshowFragment.java
@@ -6,29 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.fiek.hapirri.R;
 import com.fiek.hapirri.constants.Constants;
 import com.fiek.hapirri.model.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SlideshowFragment extends Fragment {
@@ -68,7 +60,6 @@ public class SlideshowFragment extends Fragment {
                         DocumentSnapshot document = task.getResult();
                         if (document.exists()) {
                             User loggedUser = document.toObject(User.class);
-
 
                             String firstLastName = loggedUser.getFirstName() + " " + loggedUser.getLastName();
 
